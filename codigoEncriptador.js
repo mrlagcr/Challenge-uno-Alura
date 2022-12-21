2bb1724b630a169282e0cf0e68708c5e87660776
mrlagcr <corralesbrian.bc@hotmail.com>
2022-12-21 09:02:36 -0600

    Se generaron los documentos html y js se monto la primera logica y la estructura inicial de html

---
+++ codigoEncriptador.js
@@ -0,0 +1,33 @@
+//var letra1 = "a";
+//var letra2 = "e";
+//var letra3 = "i";
+//var letra4 = "o";
+//var letra5 = "u";
+
+var letras = prompt("escriba lo que quieras")
+//Prompt me escribio todo
+//Pensar en la logica de como se imprima solo lo que se escribe junto a la conversion de cada vocal
+
+var conversion1 = "ai";
+var conversion2 = "enter";
+var conversion3 = "imes";
+var conversion4 = "ober";
+var conversion5 = "ufat";
+
+
+function convertir (){
+    if( letras = "a"){
+        document.write(conversion1);
+    }if( letras = "e"){
+        document.write(conversion2);
+    }if( letras = "i"){
+        document.write(conversion3);
+    }if(letras = "o"){
+        document.write(conversion4);
+    }if( letras = "u"){
+        document.write(conversion5);
+    }
+ }
+//posiblemente se requiera cambiar logica de la funcion
+ convertir();
+
