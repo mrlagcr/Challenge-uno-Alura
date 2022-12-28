bc28b6c021f4a21310ac952749b8f359d98236b6
mrlagcr <corralesbrian.bc@hotmail.com>
2022-12-28 09:36:59 -0600

    use un replace pero no logro cambiar u texto solo vovcal por vocal

--- codigoEncriptador.js
+++ codigoEncriptador.js
@@ -2,19 +2,21 @@ var escritura = document.querySelector("input");
 var letras = ["a", "e", "i", "o", "u"];
 var conversiones = ["ai", "enter", "imes", "ober", "ufat"];
 
+
 function imprimir(frase){
     document.write(frase);
 }
 
-function convertir(){
+function encriptar(){
     for(var posicion = 0; posicion < 5; posicion++ ){
-        if(escritura.value == letras[posicion]){
-            imprimir(conversiones[posicion]);
+        if(escritura.value  == letras[posicion]){
+           escritura = escritura.value.replace(letras[posicion], conversiones[posicion]);
         }
     }
+     imprimir(escritura);
 }
 var button = document.querySelector("button");
-button.onclick = convertir;
+button.onclick = encriptar;
 
 //Ahora hay que encontrar la forma que haga push o cambie la letra en el string ya que solo me hace cambios si escribo la vocal en especifico
 
