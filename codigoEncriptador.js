be5c33819af67051e498e12a52a2ed7b5500e699
mrlagcr (brian) <corralesbrian.bc@hotmail.com>
2022-12-28 18:40:52 -0600

    se logro los codigos de encriptado y desencriptado

--- codigoEncriptador.js
+++ codigoEncriptador.js
@@ -1,4 +1,4 @@
-var escritura = document.querySelector("input");
+
 var letras = ["a", "e", "i", "o", "u"];
 var conversiones = ["ai", "enter", "imes", "ober", "ufat"];
 
@@ -8,15 +8,38 @@ function imprimir(frase){
 }
 
 function encriptar(){
-    for(var posicion = 0; posicion < 5; posicion++ ){
-        if(escritura.value  == letras[posicion]){
-           escritura = escritura.value.replace(letras[posicion], conversiones[posicion]);
-        }
+    var escritura = document.querySelector(".input-entrada").value;
+    var escrituraEncriptada = escritura.replace(letras[1], conversiones[1]).replace(letras[2], conversiones[2]).replace(letras[3], conversiones[3]).replace(letras[0], conversiones[0]).replace(letras[4], conversiones[4]);
+    for (var posicion = 0; posicion < 5; posicion++){
+       if(letras[posicion] = conversiones[posicion]){
+            document.querySelector(".input-entrada").value = escrituraEncriptada; 
+            imprimir(escrituraEncriptada);        
+            break;
+       } 
+        
     }
-     imprimir(escritura);
+    
 }
-var button = document.querySelector("button");
+
+var button = document.querySelector(".buton-entrada");
 button.onclick = encriptar;
+
+function desencriptar(){
+    var escritura = document.querySelector(".input-salida").value;
+    var escrituraEncriptada = escritura.replace(conversiones[1], letras[1]).replace(conversiones[2], letras[2]).replace(conversiones[3], letras[3]).replace(conversiones[0], letras[0]).replace(conversiones[4], letras[4]);
+    for (var posicion = 0; posicion < 5; posicion++){
+       if(letras[posicion] = conversiones[posicion]){
+            document.querySelector(".input-salida").value = escrituraEncriptada; 
+            imprimir(escrituraEncriptada);        
+            break;
+       } 
+        
+    }
+    
+}
+
+var button = document.querySelector(".buton-salida");
+button.onclick = desencriptar;
 
 //Ahora hay que encontrar la forma que haga push o cambie la letra en el string ya que solo me hace cambios si escribo la vocal en especifico
 
